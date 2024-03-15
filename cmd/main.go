9b5fca41c851bee4b30bda523217712eb8ce41ea
Filip Pavlovic <filpavlovic@gmail.com>
2024-03-15 22:54:26 +0100

    feat(index): self disabling buttons, one theme changing button (wip)

--- cmd/main.go
+++ cmd/main.go
@@ -27,6 +27,8 @@ func newTemplate() *Templates {
 type State struct {
     Count int
     temp *Templates
+    Left bool
+    Theme string
 }
 
 func (c *State) Plus(w http.ResponseWriter, r *http.Request) {
@@ -38,15 +40,33 @@ func (c *State) Index(w http.ResponseWriter, r *http.Request) {
     c.temp.Render(w, "index", c)
 }
 
+func (c *State) Buttons(w http.ResponseWriter, r *http.Request) {
+    c.Left = !c.Left
+    c.temp.Render(w, "buttons", c)
+}
+
+func (c *State) SwapTheme(w http.ResponseWriter, r *http.Request) {
+    if c.Theme == "dark" {
+        c.Theme = "light"
+    } else {
+        c.Theme = "dark"
+    }
+    c.temp.Render(w, "theme", c)
+}
+
 func main() {
     st := State { 
         Count: 0,
         temp: newTemplate(),
+        Left: true,
+        Theme: "dark",
     }
 
     mux := http.NewServeMux()
     mux.HandleFunc("GET /", st.Index)
     mux.HandleFunc("POST /count", st.Plus)
+    mux.HandleFunc("POST /buttons", st.Buttons)
+    mux.HandleFunc("POST /theme", st.SwapTheme)
     srv := &http.Server {
         Addr: ":8090",
         Handler: mux,
