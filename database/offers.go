c55457b21a831581194154c0288252872f8e448b
Filip Pavlovic <filpavlovic@gmail.com>
2024-04-13 19:23:57 +0200

    feat(db): basic Offer handling

--- database/offers.go
+++ database/offers.go
@@ -2,6 +2,7 @@ package database
 
 import (
 	"database/sql"
+    "fmt"
 
 	_ "github.com/mattn/go-sqlite3"
 )
@@ -15,6 +16,41 @@ type Offer struct {
 
 
 type Offers = []Offer
+
+func NewOffer(id, id_owner int, name, desc string) (*Offer) {
+    return &Offer{
+        ID: id,
+        ID_owner: id_owner,
+        Name: name,
+        Description: desc,
+    }
+}
+
+func (dbh *DBHandler) InsertOffer(of *Offer) error {
+    _, err := dbh.DB.Exec(`
+    INSERT INTO Offers (ID, ID_owner, Name, Description) 
+    VALUES (?, ?, ?, ?)`, 
+    of.ID, of.ID_owner, of.Name, of.Description)
+    fmt.Printf("of.id: %v\n", of.ID)
+    return err
+}
+
+func (dbh *DBHandler) GetOffers() (Offers, error) {
+    rows, err := dbh.DB.Query("SELECT (*) FROM" + dbh.Offers)
+    if err != nil {
+        fmt.Printf("get err.Error(): %v\n", err.Error())
+        return nil, err
+    }
+	defer rows.Close()
+
+	var offers Offers
+	for rows.Next() {
+		var offer Offer
+		rows.Scan(&offer.ID, &offer.ID_owner, &offer.Name, &offer.Description)
+		offers = append(offers, offer)
+	}
+	return offers, nil
+}
 
 
 
