1298a8f8bec15d5b596866eadd66daec8ebb1fba
Filip Pavlovic <filpavlovic@gmail.com>
2024-05-19 13:43:27 +0200

    feat(install): oops

--- install/create.sh
+++ install/create.sh
@@ -3,4 +3,4 @@
 cd ..
 make
 cd install
-zip install.zip Makefile.am bazos admin.txt.in base_pfp.png configure.ac NEWS README AUTHORS ChangeLog autogen.sh
+zip install.zip Makefile.am bazos admin.txt.in ../images/base_pfp.png configure.ac NEWS README AUTHORS ChangeLog autogen.sh
